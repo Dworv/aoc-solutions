53b52f1a245e5f95c510f250fb60ef1869ca0634
Dworv <dwarvyt@gmail.com>
2023-12-05 18:49:18 -0800

    DAY 5 PART 2 YIPPEEEEEE

--- 2023/day-5/src/main.rs
+++ 2023/day-5/src/main.rs
@@ -23,50 +23,45 @@ fn part_1(input: &str) {
 fn part_2(input: &str) {
     let Input { seeds: items, maps } = Input::parse(input);
     let mut ranges = items.chunks(2).map(|x| x[0]..(x[0] + x[1])).collect::<Vec<_>>();
-    println!("ranges sum: {}", ranges.iter().map(|x| x.end - x.start).sum::<u64>());
+    println!("ranges sum: {}", ranges.iter().map(|x| x.end - x.start).sum::<i64>());
     for mut map in maps {
         map.sort_by_key(|(_, start, _)| *start);
         ranges = ranges.iter().flat_map(|range| {
-            let mut new_ranges = vec![range.clone()];
-
+            let mut unmapped = vec![range.clone()];
+            let mut mapped = vec![];
             for (dest, source, len) in map.iter() {
-                let starts = new_ranges.clone();
                 let catch_range = *source..(*source + len);
-                let dest_range = *dest..(*dest + len);
-                if catch_range.start <= range.start && catch_range.end >= range.end {
-                    new_ranges[0] = (dest_range.start + (range.start - catch_range.start))..(dest_range.start + (range.end - catch_range.start));
-                }
-                else if catch_range.end > range.start && catch_range.start <= range.start {
-                    new_ranges[0] = (dest_range.end - (catch_range.end - range.start))..dest_range.end;
-                    new_ranges.push(catch_range.end..range.end);
-                }
-                else if catch_range.start >= range.start && catch_range.end <= range.end {
-                    if catch_range.start > new_ranges.last().unwrap().start {
-                        new_ranges.last_mut().unwrap().end = catch_range.start;
+                let offset = dest - source;
+                let mut new_unmapped = vec![];
+                for og in unmapped {
+                    if catch_range.start <= og.start && og.end <= catch_range.end {
+                        mapped.push((og.start + offset)..(og.end + offset));
                     }
-                    new_ranges.push(dest_range.clone());
-                    if range.end > catch_range.end {
-                        new_ranges.push(catch_range.end..range.end);
+                    else if og.start < catch_range.end && catch_range.start <= og.start {
+                        mapped.push((og.start + offset)..(catch_range.end + offset));
+                        new_unmapped.push(catch_range.end..range.end);
                     }
-                }
-                else if catch_range.start < range.end && catch_range.end >= range.end {
-                    new_ranges.last_mut().unwrap().end = catch_range.start;
-                    new_ranges.push(dest_range.start..(dest_range.start + (range.end - catch_range.start)));
-                }
-                let mut copy = new_ranges.clone();
-                copy.sort_by_key(|x| x.start);
-                for i in 0..(copy.len() - 1) {
-                    if copy[i].end > copy.get(i+1).unwrap_or(&(100000000..100000000)).start {
-                        println!("FAUILURE FOUND \n MAP: {:?} {:?} -> {:?} \n STARTS: {:?} \n ENDS: {:?}\n", (dest, source, len), catch_range, dest_range, starts, new_ranges);
+                    else if range.start < catch_range.start && catch_range.end < range.end {
+                        new_unmapped.push(range.start..catch_range.start);
+                        mapped.push((catch_range.start + offset)..(catch_range.end + offset));
+                        new_unmapped.push(catch_range.end..range.end);
+                    }
+                    else if catch_range.start < range.end && catch_range.end >= range.end {
+                        new_unmapped.push(range.start..catch_range.start);
+                        mapped.push((catch_range.start + offset)..(range.end +  offset));
+                    } else {
+                        new_unmapped.push(og);
                     }
                 }
+                unmapped = new_unmapped;
             }
-
-            new_ranges
+            
+            mapped.extend(unmapped);
+            mapped
 
         }).collect::<Vec<_>>();
 
-        println!("ranges sum: {}", ranges.iter().map(|x| x.end - x.start).sum::<u64>());
+        println!("ranges sum: {}", ranges.iter().map(|x| x.end - x.start).sum::<i64>());
     }
     println!("ranges: {:?}", ranges);
     println!("min: {:?}", ranges.iter().min_by_key(|x| x.start).unwrap().start);
@@ -74,8 +69,8 @@ fn part_2(input: &str) {
 
 #[derive(Debug)]
 struct Input {
-    seeds: Vec<u64>,
-    maps: Vec<Vec<(u64, u64, u64)>>
+    seeds: Vec<i64>,
+    maps: Vec<Vec<(i64, i64, i64)>>
 }
 
 impl Input {
@@ -86,7 +81,7 @@ impl Input {
             .unwrap()
             .split(": ").nth(1)
             .unwrap().split(' ')
-            .map(|x| x.parse::<u64>().unwrap())
+            .map(|x| x.parse::<i64>().unwrap())
             .collect::<Vec<_>>();
         let mut maps = vec![];
         lines.next();
@@ -105,9 +100,9 @@ impl Input {
                 }
                 let mut parts = line.split(' ');
                 map.push((
-                    parts.next().unwrap().parse::<u64>().unwrap(),
-                    parts.next().unwrap().parse::<u64>().unwrap(),
-                    parts.next().unwrap().parse::<u64>().unwrap(),
+                    parts.next().unwrap().parse::<i64>().unwrap(),
+                    parts.next().unwrap().parse::<i64>().unwrap(),
+                    parts.next().unwrap().parse::<i64>().unwrap(),
                 ));
             }
             maps.push(map);
