46bc14e193485ba41aeb7b4294e05d657a907cf3
Dworv YT <dwarvyt@gmail.com>
2023-12-12 13:29:25 -0800

    day 11 doene

--- 2023/day-11/src/main.rs
+++ 2023/day-11/src/main.rs
@@ -4,15 +4,36 @@ fn main() {
     let input = rtg(11);
 
     part_1(&input);
+    part_2(&input);
 }
 
 fn part_1(input: &CharGrid) {
-    let galaxies = calc_galaxies(input);
+    let galaxies = calc_galaxies(input, 1);
 
-    dbg!(galaxies);
+    let mut sum = 0;
+    for i in 0..galaxies.len() {
+        for j in (i+1)..galaxies.len() {
+            sum += galaxies[i].0.abs_diff(galaxies[j].0) + galaxies[i].1.abs_diff(galaxies[j].1);
+        }
+    }
+
+    println!("Result: {}", sum);
 }
 
-fn calc_galaxies(input: &CharGrid) -> Vec<(u64, u64)> {
+fn part_2(input: &CharGrid) {
+    let galaxies = calc_galaxies(input, 999999);
+
+    let mut sum = 0;
+    for i in 0..galaxies.len() {
+        for j in (i+1)..galaxies.len() {
+            sum += galaxies[i].0.abs_diff(galaxies[j].0) + galaxies[i].1.abs_diff(galaxies[j].1);
+        }
+    }
+
+    println!("Result: {}", sum);
+}
+
+fn calc_galaxies(input: &CharGrid, distance: u64) -> Vec<(u64, u64)> {
     let mut galaxies = vec![];
 
     let mut empties = 0;
@@ -21,7 +42,7 @@ fn calc_galaxies(input: &CharGrid) -> Vec<(u64, u64)> {
         for x in 0..input.num_cols() {
             if input.get(x, y).unwrap() == '#' {
                 is_empty = false;
-                galaxies.push((x as u64, y as u64 + empties))
+                galaxies.push((x as u64, y as u64 + empties * distance))
             }
         }
 
@@ -40,7 +61,7 @@ fn calc_galaxies(input: &CharGrid) -> Vec<(u64, u64)> {
             last = galaxy.0 as i64;
         }
 
-        galaxy.0 += empties as u64;
+        galaxy.0 += empties as u64 * distance;
     }
 
     galaxies
